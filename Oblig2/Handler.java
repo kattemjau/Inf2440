167804fdf5423d322af1f2c9667502e4e12cc02f
kattemjau <sindre.c.h@hotmail.com>
2017-02-16 14:04:49 +0100

    yes

--- Oblig2/Handler.java
+++ Oblig2/Handler.java
@@ -1,28 +1,26 @@
 import java.util.ArrayList;
 
 public class Handler{
-	public ArrayList<Integer> array = new ArrayList<>();
+	private ArrayList<Integer> array = new ArrayList<>();
+	private int[] erastothenes;
 
 	Handler(){
 
 	}
-	void finnPrimtall(int antPrimtall){
+	void finnPrimtall(int maxtall){
 		//sorterings algorithme for a finne primtall
 		// kun 2 er enseste partall som er primtall
 		// for alle oddetall, sjekk om primtall
+		maxtall=maxtall*maxtall;
+		long antPrimtall = 4000000000000L;
 		System.out.println("tester for antPrimtall: " + antPrimtall);
-
-		for (int i = 3; i<antPrimtall; i+=2) {
-			// System.out.print(i +" = ");
-			faktorisering(i);
-			// System.out.println(" ");
-			
+		long tid = System.nanoTime();
+		for (long i=antPrimtall-100;  i<antPrimtall; i++) {
+			System.out.print(i + " = ");
+			faktorisering(i);		
+			System.out.println(" ");	
 		}
-
-		for (Integer e: array) {
-			System.out.println(e);
-		}
-
+		System.out.println("tid pa sekvensiel faktorisering: " + ((System.nanoTime()-tid)/1000000.0) + " ms");
 
 	}
 
@@ -32,33 +30,59 @@ public class Handler{
 // if modula == 0 || primtall = true
 // rekursiv faktorisering if(n%i != 0){ System.out.print(n/i + " + "); rekursiv(n/i)}
 
-	void faktorisering(int potensielPrim){
+	void faktorisering(long maxtall){
 		//denne skal multithreades "tar lengst tid"
-		
-		for(int i=2; i<potensielPrim; i++){
-			// System.out.println("factorisererer : " + i);
-			if(potensielPrim%i == 0){
-				// System.out.print(potensielPrim/i);
-				if(array.contains(potensielPrim/i)){
-					// System.out.print(potensielPrim);
-					return;
-				}
-				// System.out.print(" * ");
-				faktorisering(potensielPrim/i);
+		for(Integer i: array){
+			if(maxtall%i == 0){
+				System.out.print(i + " * ");
+				faktorisering(maxtall/i);
 				return;
 			}
-
 		}
-		// System.out.println("DEN KJEM HIT");
-		// kommer den hit er det et primtall
-		//add primtall
-		// if(!array.contains(potensielPrim)){
-		array.add(potensielPrim);
-		// System.out.print(potensielPrim);
+		System.out.print(maxtall);
+	}
 
+	void eratosthenesSil(int maxtall){
+		System.out.println("eratosthenesSil");
+		opprettArray(maxtall);
+
+		long ti = System.nanoTime();
+		for(Integer e: erastothenes){
+			// System.out.println("testing for "+ e);
+			if(e<10 && e!=0){
+				int y=2;
+				for(int i=y*e; i<maxtall; i=(++y*e)){
+					erastothenes[i-2]=0;
+					// System.out.println("fjerner: " + i);
+					// System.out.println("Y = " + y);
+				}
+			}else if(e>10){
+				break;
+			}
+		}
+		System.out.println("tid pa eratosthenesSil: " + ((System.nanoTime()-ti)/1000000.0) + " ms");
+
+		for(Integer k: erastothenes){
+			if(k!=0){
+			// System.out.println("primtall: " + k);
+				array.add(k);
+			}
+		}
+		
+
+		// for(int i=0;i<maxtall;i++){
+		// 	System.out.println(erastothenes[i]);
 		// }
 
+	}
 
+	void opprettArray(int maxtall){
+		erastothenes=new int[maxtall];
+
+		for(int i=2; i<maxtall; i++){
+			erastothenes[i-2]=i;
+			// System.out.println(i);
+		}
 	}
 
 
--- Oblig2/Oblig2.java
+++ Oblig2/Oblig2.java
@@ -2,10 +2,14 @@
 public class Oblig2{
 	public static void main(String[] args) {
 		try{
-		int antPrimtall = Integer.parseInt(args[0]);
-		System.out.println("thiws" + antPrimtall);
+		int antTall = Integer.parseInt(args[0]);
 		Handler hand = new Handler();
-		hand.finnPrimtall(antPrimtall);
+		// System.out.println(antTall);
+
+		hand.eratosthenesSil(antTall);
+		hand.finnPrimtall(antTall);
+
+
 			
 		}catch (NullPointerException e){
 			System.out.println("Usage: java Oblig2 'numbers to iterate over'");
