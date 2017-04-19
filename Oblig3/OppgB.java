e9aa4c53c6ef620f70002ef7e0a39a9bdaf97d8d
kattemjau <sindre.c.h@hotmail.com>
2017-04-19 20:44:54 +0200

    tnhis

--- Oblig3/OppgB.java
+++ Oblig3/OppgB.java
@@ -1,22 +1,39 @@
 class OppgB extends Thread{
   int start, slutt, shift, mask;
-  int[] count, a, b;
+  int[] count, a; 
+  int[][] b;
+  int index;
 
-  public OppgB(int index, int start, int slutt, int[] count, int[] a, int[] b, int shift, int mask){
-    this.count=count;
+
+  public OppgB(int index, int start, int slutt, int[] a, int[][] b, int shift, int mask){
+    // this.count=count;
+    this.index=index;
     this.start=start;
+    count = new int [mask+1];
     this.shift=shift;
     this.mask=mask;
-		this.slutt=slutt;
+    this.slutt=slutt;
     this.a=a;
     this.b=b;
   }
   public void run(){
+    // System.out.println("index " + index);
     int acumVal=0, j;
-    for (int i =start; i <slutt; i++) { //b finn allcount
-			count[(a[i]>>> shift) & mask]++;
-		}
+    // System.out.println("Mask:" + mask + ", skift: " + shift);
 
+    for (int i =start; i <slutt; i++) { //b finn allcount
+      count[(a[i]>>> shift) & mask]++;
+      
+    }
+    // for (int i = 0; i < count.length; i++) {
+    //   System.out.print(count[i] + " y ");
+      
+    // }
+    for (int i=0;i<count.length ;i++ ) {
+      b[index][i]=count[i];
+      // allcount[count[i]]++;
+    }
+   
 
 
 
--- Oblig3/Sekvensiell.java
+++ Oblig3/Sekvensiell.java
@@ -34,69 +34,122 @@ class Sekvensiell{
 
 	int max;
 
-	int[]  radixMulti(int [] a) {
-		// 1-5 digit radixSort of : a[]
-		int numBit = 2, numDigits, n =a.length;
-		int [] bit ;
-		int[] t=a, b = new int [n];
-		max=a[0];
+	// int[]  radixMulti(int [] a) {
+	// 	// 1-5 digit radixSort of : a[]
+	// 	int numBit = 2, numDigits, n =a.length;
+	// 	int [] bit ;
+	// 	int[] t=a, b = new int [n];
+	// 	max=a[0];
 
-		OppgA[] wut = new OppgA[cores];
+	// 	// OppgA[] wut = new OppgA[cores];
 
-		long tt = System.nanoTime();
-		int nr=n/cores;
-		int start=1, slutt=nr+n%cores;
-		for(int i=0;i<cores;i++){
-			wut[i]=new OppgA(start, slutt, a);
-			wut[i].start();
-			start=slutt;
-			slutt=start+nr;
-		}
+	// 	long tt = System.nanoTime();
+	// 	// int nr=n/cores;
+	// 	// int start=1, slutt=nr+n%cores;
+	// 	// for(int i=0;i<cores;i++){
+	// 	// 	wut[i]=new OppgA(start, slutt, a);
+	// 	// 	wut[i].start();
+	// 	// 	start=slutt;
+	// 	// 	slutt=start+nr;
+	// 	// }
 
-		for(OppgA e: wut){
-			try{
-				e.join();
-				if(e.max>max)max=e.max;
-			}catch(Exception y){
-			}
-		}
+	// 	// for(OppgA e: wut){
+	// 	// 	try{
+	// 	// 		e.join();
+	// 	// 		if(e.max>max)max=e.max;
+	// 	// 	}catch(Exception y){
+	// 	// 	}
+	// 	// }
+	// 	// System.out.println("MAxverdi: " + max);
+		
+	// 		//A)
+	// 	for (int i = 1 ; i < n ; i++)
+	// 		   if (a[i] > max) max = a[i];
+	// 	  // while (max >= (1L<<numBit) )numBit++; // antall binaere siffer i max
 
-		while (max >= (1L<<numBit) )numBit++; // antall binaere siffer i max
+	// 	while (max >= (1L<<numBit) )numBit++; // antall binaere siffer i max
 
-		// bestem antall bit i numBits sifre
-		numDigits = Math.max(1, numBit/NUM_BIT);
-		bit = new int[numDigits];
-		int rest = (numBit%numDigits), sum =0;;
+	// 	// bestem antall bit i numBits sifre
+	// 	numDigits = Math.max(1, numBit/NUM_BIT);
+	// 	bit = new int[numDigits];
+	// 	int rest = (numBit%numDigits), sum =0;;
 
-		// fordel bitene vi skal sortere paa jevnt
-		for (int i = 0; i < bit.length; i++){
-			bit[i] = numBit/numDigits;
-			if ( rest-- > 0)  bit[i]++;
-		}
+	// 	// fordel bitene vi skal sortere paa jevnt
+	// 	for (int i = 0; i < bit.length; i++){
+	// 		bit[i] = numBit/numDigits;
+	// 		if ( rest-- > 0)  bit[i]++;
+	// 	}
 
 
-		for (int i =0; i < bit.length; i++) {
-			radixSort( a,b,bit[i],sum );    // i-te siffer fra a[] til b[]
-			sum += bit[i];
-			// swap arrays (pointers only)
-			t = a;
-			a = b;
-			b = t;
-		}
-		// 	for(int i=0; i<count.length;i++)	System.out.println(count[i]);
-		// //erstatt denne
-		if (bit.length%2 == 0 ) {
-		  // et odde antall sifre, kopier innhold tilbake til original a[] (nå b)
-			System.arraycopy (b,0,a,0,a.length);
-		}
+	// 	for (int i =0; i < bit.length; i++) {
+	// 		radixSort( a,b,bit[i],sum );    // i-te siffer fra a[] til b[]
+	// 		sum += bit[i];
+	// 		// swap arrays (pointers only)
+	// 		t = a;
+	// 		a = b;
+	// 		b = t;
+	// 	}
+	// 	// 	for(int i=0; i<count.length;i++)	System.out.println(count[i]);
+	// 	// //erstatt denne
+	// 	if (bit.length%2 == 0 ) {
+	// 	  // et odde antall sifre, kopier innhold tilbake til original a[] (nå b)
+	// 		System.arraycopy (b,0,a,0,a.length);
+	// 	}
 
-		double tid = (System.nanoTime() -tt)/(double)1000000.0;
-		System.out.println("\nSorterte "+n+" tall paa:" + tid + "millisek.");
-		System.out.println("Speedup for n=" + n + " Speedup: " + (sekvtid/tid));
-		testSort(a);
-		return a;
-	} // end radixMulti
-	/** Sort a[] on one digit ; number of bits = maskLen, shiftet up 'shift' bits */
+	// 	double tid = (System.nanoTime() -tt)/(double)1000000.0;
+	// 	System.out.println("\nSorterte "+n+" tall paa:" + tid + "millisek.");
+	// 	System.out.println("Speedup for n=" + n + " Speedup: " + (sekvtid/tid));
+	// 	testSort(a);
+	// 	return a;
+	// } // end radixMulti
+	
+	int[] radixMulti(int [] a) {
+		  // 1-5 digit radixSort of : a[]
+		  int max = a[0], numBit = 2, numDigits, n =a.length;
+		  int [] bit ;
+		  long tt = System.nanoTime();
+
+		 // a) finn max verdi i a[]
+		  for (int i = 1 ; i < n ; i++)
+			   if (a[i] > max) max = a[i];
+		  while (max >= (1L<<numBit) )numBit++; // antall binaere siffer i max
+
+		  // bestem antall bit i numBits sifre
+		  numDigits = Math.max(1, numBit/NUM_BIT);
+		  bit = new int[numDigits];
+		  int rest = (numBit%numDigits), sum =0;;
+
+		  // fordel bitene vi skal sortere paa jevnt
+		  for (int i = 0; i < bit.length; i++){
+			  bit[i] = numBit/numDigits;
+		      if ( rest-- > 0)  bit[i]++;
+		  }
+
+		  int[] t=a, b = new int [n];
+
+		  for (int i =0; i < bit.length; i++) {
+			  radixSort( a,b,bit[i],sum );    // i-te siffer fra a[] til b[]
+			  sum += bit[i];
+			  // swap arrays (pointers only)
+			  t = a;
+			  a = b;
+			  b = t;
+		  }
+		  if (bit.length%2 != 0 ) {
+			  // et odde antall sifre, kopier innhold tilbake til original a[] (nå b)
+			  System.arraycopy (a,0,b,0,a.length);
+		  }
+
+		  double tid = (System.nanoTime() -tt)/(double)1000000.0;
+		  System.out.println("\nSorterte "+n+" tall paa:" + tid + "millisek.");
+		  System.out.println("Speedup for n=" + n + " Speedup: " + (sekvtid/tid));
+
+		  testSort(a);
+		  return a;
+	 } // end radixMulti
+
+	int[][] dobbelArray;
+
 	void radixSort ( int [] a, int [] b, int maskLen, int shift){
 		// System.out.println(" radixSort maskLen:"+maskLen+", shift :"+shift);
 		int  acumVal = 0, j, n = a.length;
@@ -104,53 +157,74 @@ class Sekvensiell{
 		int[] count = new int [mask+1];
 
 		// b) count=the frequency of each radix value in a
-		for (int i = 0; i < n; i++) {
-		 count[(a[i]>>> shift) & mask]++;
-		}
-		//
-		// int nr=n/cores;
-		// 	int start=0, slutt=nr+n%cores;
-		// 	OppgB[] array = new OppgB[cores];
-		//
-		// 	for (int w =0; w < cores; w++) {
-	 // 		array[w]= new OppgB(w, start, slutt, Arrays.copyOf(count, count.length), a, b, shift, mask);
-	 // 		array[w].start();
-		// 	start=slutt;
-		// 	slutt=start+nr;
-		//
+		// for (int i = 0; i < n; i++) {
+		// 	count[(a[i]>>> shift) & mask]++;
 		// }
-
-
-		// c) Add up in 'count' - accumulated values
-		// for (int i = 0; i <= mask; i++){
-		// 	j = count[i];
-		// 	count[i] = acumVal;
-		// 	acumVal += j;
-		// }
-		//debug print av hele count. burde vere i stigende rekkefolge.
-
-		// d) move numbers in sorted order a to b
-			// for (int i = 0; i < n; i++) {
-			// 	b[count[(a[i]>>>shift) & mask]++] = a[i];
-			// }
-		// //
+		
 		int nr=n/cores;
-		int rest= n%cores;
-		int start=0, slutt=nr+rest;
-		Traad[] array = new Traad[cores];
-
+		int start=0, slutt=nr+n%cores;
+		OppgB[] array = new OppgB[cores];
+		
+	    int[] allcount= new int[mask+1];
+		dobbelArray=new int[cores][slutt];
 		for (int w =0; w < cores; w++) {
-			array[w]= new Traad(this, a, b, start, slutt,count, shift, mask);
+			array[w]= new OppgB(w, start, slutt, a, dobbelArray, shift, mask);
+			// System.out.println("index: " + w + " start: " + start + " stopp: " + slutt);
 			array[w].start();
 			start=slutt;
 			slutt=start+nr;
+
 		}
-		for(Traad e: array){
+
+		//venter pa traader
+		for(OppgB e: array){
 			try{
 				e.join();
 			}catch(Exception y){
 			}
 		}
+
+		//count all instences and put in count
+		for (int i=0;i<count.length ;i++ ) {
+			for (int w =0; w < cores; w++) {
+				count[i]+=dobbelArray[w][i];
+			}
+		}
+		// c) Add up in 'count' - accumulated values
+		for (int i = 0; i <= mask; i++){
+			j = count[i];
+			count[i] = acumVal;
+			acumVal += j;
+		}
+		//prints count
+		// for (int i = 0; i < count.length; i++) {
+		// 	System.out.print(count[i] + " ");
+      
+		// }
+		// debug print av hele count. burde vere i stigende rekkefolge.
+
+		// d) move numbers in sorted order a to b
+		for (int i = 0; i < n; i++) {
+			b[count[(a[i]>>>shift) & mask]++] = a[i];
+		}
+		//
+		// int nr=n/cores;
+		// int rest= n%cores;
+		// int start=0, slutt=nr+rest;
+		// Traad[] array = new Traad[cores];
+
+		// for (int w =0; w < cores; w++) {
+		// 	array[w]= new Traad(this, a, b, start, slutt,count, shift, mask);
+		// 	array[w].start();
+		// 	start=slutt;
+		// 	slutt=start+nr;
+		// }
+		// for(Traad e: array){
+		// 	try{
+		// 		e.join();
+		// 	}catch(Exception y){
+		// 	}
+		// }
 
 	}// end radixSort
 
@@ -158,7 +232,7 @@ class Sekvensiell{
 	void testSort(int [] a){
 		for (int i = 0; i< a.length-1;i++) {
 			if (a[i] > a[i+1]){
-				System.out.println("SorteringsFEIL på plass: "+i +" a["+i+"]:"+a[i]+" > a["+(i+1)+"]:"+a[i+1]);
+				System.out.println("SorteringsFEIL pa plass: "+i +" a["+i+"]:"+a[i]+" > a["+(i+1)+"]:"+a[i+1]);
 				return;
 			}
 		}
